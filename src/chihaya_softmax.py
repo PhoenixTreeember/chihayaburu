92486d5f67393e080a6a558d3048f4dfa6a1266e
natsutan <yrw03704@nifty.com>
2016-10-28 10:40:37 +0900

    学習結果の保存と読み込みに対応

--- src/chihaya_softmax.py
+++ src/chihaya_softmax.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """A very simple MNIST classifier.
 
 See extensive documentation at
