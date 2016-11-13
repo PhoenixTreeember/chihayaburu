2feee548dd22e1df39fcdaf3b0eceeb5f4c0a2b0
natsutan <yrw03704@nifty.com>
2016-11-13 14:38:37 +0900

    画像データダンプ

---
+++ c_imple/softmax/img/conv_img.py
@@ -0,0 +1,30 @@
+import numpy as np
+from PIL import Image
+from PIL import ImageOps
+
+input_dir = 'png'
+output_dir = 'output'
+files = ['A.png', 'I.png', 'U.png', 'E.png', 'O.png']
+
+def write_data(f, mtx):
+  fname = output_dir + '/' + f + 'txt'
+  with open(fname, 'w') as fp:
+    for v in mtx:
+      fp.write("%02X\n" % int(v))
+
+  print("write %s" % fname)
+
+
+for f in files:
+  fname = input_dir + '/' + f
+  img_ori = Image.open(fname)
+  img_gray = ImageOps.grayscale(img_ori)
+
+
+  # imageををnumpyに変更して、imagesに追加
+  img_ary = np.asarray(img_gray)
+  img_flat = img_ary.flatten().astype(np.float32)
+
+  write_data(f, img_flat)
+
+
