75ed348d3719db4619846ece3e26cee7c0c6df51
natsutan <YRW03704@nifty.com>
2016-11-24 13:30:09 +0900

    CNNパラメータのテキスト化

--- c_imple/cnn/param/npy2txt.py
+++ c_imple/cnn/param/npy2txt.py
@@ -31,6 +31,28 @@ def wc2proc(input, output):
           fp.write('\n')
         i+=1
 
+
+def bc1proc(input, output):
+  arr = np.load(input)
+  with open(output_dir + '/' + output, 'w') as fp:
+    for a in arr:
+      fp.write("%f\n" % a)
+
+def wfc1proc(input, output):
+  arr = np.load(input)
+  i = 0
+  for a in arr.T:
+    with open(output_dir + '/' + output + ("%04d" % i) + ".txt", 'w') as fp:
+      for b in a:
+          fp.write("%f\n" % b)
+    i+=1
+
 #wc1proc('wc1.npy', 'wc1.txt')
 #wc2proc('wc2.npy', 'wc2.txt')
+#bc1proc('bc1.npy', 'bc1.txt')
+#bc1proc('bc2.npy', 'bc2.txt')
+#bc1proc('bfc1.npy', 'bfc1.txt')
+#bc1proc('bfc2.npy', 'bfc2.txt')
 
+#wfc1proc('wfc1.npy', 'wfc1/wfc1_')
+wfc1proc('wfc2.npy', 'wfc2/wfc2_')
